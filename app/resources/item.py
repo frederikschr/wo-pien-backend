36b8a2ba131b2e0e17070f1e49a78a0eaefaa91c
Frederik Schier <fredi.schier@gmail.com>
2022-04-18 13:49:23 +0200

    items updated

--- app/resources/item.py
+++ app/resources/item.py
@@ -11,34 +11,44 @@ class ItemBringResource(Resource):
     def patch(self):
         json_data = request.get_json()
         user = User.query.get(get_jwt_identity())
+        error = None
         for item in json_data["updated_items"]:
             item_db = Item.query.get(item["id"])
-            if item_db and user in item_db.session.members:
-                if item_db.byHost and user.id != item_db.session.owner_id:
-                    return HTTPStatus.UNAUTHORIZED
+            if item_db:
+                if user in item_db.session.members:
+                    if not item_db.byHost and user.id == item_db.session.owner_id:
+                        item_db.byHost = True
 
-                elif not item_db.byHost and user.id == item_db.session.owner_id:
-                    item_db.byHost = True
+                    if member_item := MemberItems.query.get((user.id, item_db.id)):
+                        if not member_item.item_amount == item["bring_amount"] or not member_item.item_price == item["price"]:
+                            item_db.amount_brought += (item["bring_amount"] - member_item.item_amount)
+                            member_item.item_price = item["price"]
+                            member_item.item_amount = item["bring_amount"]
 
-                if member_item := MemberItems.query.get((user.id, item_db.id)):
-                    member_item.item_price = item["price"]
-                    member_item.item_amount = item["bring_amount"]
-                    if member_item.item_amount != item["bring_amount"]:
-                        if item_db.amount_brought > item["bring_amount"]:
-                            item_db.amount_brought += item["bring_amount"]
-                        else:
-                            item_db.amount_brought -= item_db.amount_brought - item["bring_amount"]
+
+                    else:
+                        member_item = MemberItems(user_id=user.id, item_id=item_db.id, item_amount=item["bring_amount"], item_price=item["price"])
+                        user.items.append(member_item)
+                        item_db.amount_brought += item["bring_amount"]
+
+                    if item_db.amount_brought > item_db.amount:
+                        item_db.amount = item_db.amount_brought
                 else:
-                    member_item = MemberItems(user_id=user.id, item_id=item_db.id, item_amount=item["bring_amount"], item_price=item["price"])
-                    user.items.append(member_item)
-                    item_db.amount_brought += item["bring_amount"]
+                    error = {"error": "You are not part of this session", "status": HTTPStatus.UNAUTHORIZED}
 
             else:
-                print("item does not exist")
+                error = {"error": "Item does not exist (anymore)", "status": HTTPStatus.NOT_FOUND}
+
+            if error:
+                return {"error": error["error"]}, error["status"]
+
+        #for item in json_data["removed_items"]:
 
         db.session.commit()
 
         return HTTPStatus.OK
+
+
 
 
 class ItemManageResource(Resource):
