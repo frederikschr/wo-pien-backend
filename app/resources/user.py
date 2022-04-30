d98cf63591fefe18e90fc716a52f40fdfd196d4f
Frederik Schier <fredi.schier@gmail.com>
2022-04-30 22:40:56 +0200

    session removal edited

--- app/resources/user.py
+++ app/resources/user.py
@@ -7,6 +7,20 @@ from .jwt import *
 import datetime as dt
 from app.models.user import *
 from app.schemas.user import UserSchema
+import time
+
+def check_sessions(user):
+    today = time.strptime(str(dt.date.today()), "%Y-%m-%d")
+    for session in user.sessions:
+        if time.strptime(session.date, "%Y-%m-%d") < today:
+            for bringing in session.member_items:
+                db.session.delete(bringing)
+
+            for item in session.items:
+                db.session.delete(item)
+
+            db.session.delete(session)
+            db.session.commit()
 
 class UserResource(Resource):
     def post(self):
@@ -31,6 +45,10 @@ class UserResource(Resource):
             if check_password_hash(user.password, password):
                 expires = dt.timedelta(hours=3)
                 access_token = create_access_token(identity=user.id, expires_delta=expires, fresh=True)
+
+                check_sessions(user)
+
+
                 return {"user": user.get_data(), "token": access_token}, HTTPStatus.OK
             else:
                 data = {"error": "Incorrect password", "status": HTTPStatus.FORBIDDEN}
