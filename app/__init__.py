3a19602f7cf068e2461bef1431637d430d162f27
Frederik Schier <fredi.schier@gmail.com>
2022-05-07 20:11:28 +0200

    heroku deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
@@ -12,7 +14,7 @@ app = Flask(__name__)
 def create_app():
     #app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SECRET_KEY"] = "secret!"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     app.config['CORS_HEADERS'] = 'Content-Type'
 
     db.init_app(app)
